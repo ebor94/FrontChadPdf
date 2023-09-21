c4e77d043afe637d5c2b20bbc1576e0a3afe57f0
ebor94 <webmaster@ceramicaitalia.com>
2023-09-21 10:15:54 -0500

    app finished

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chat = async (e, message) => {
+    while(chats.length > 0){
+      chats.pop(); 
+}
+    e.preventDefault();
+
+    if (!message) return;
+    setIsTyping(true);
+    scrollTo(0, 1e10);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+
+    setMessage("");
+
+    fetch("http://localhost:3000/sendMessage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "sourceId": "src_hAfyfAM6iwUZ7o5AC38oY",
+         "messages": chats
+    }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage("");
+        msgs.push(data);
+        setChats(msgs);
+        setIsTyping(false);
+        scrollTo(0, 1e10);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <main>
+      <h1>Chat with tibIA</h1>
+
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+                <span>
+                  <b>{chat.role}</b>
+                </span>
+                <span>TIB-IA answer: </span>
+                <span>{chat.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+
+      <div className={isTyping ? "" : "hide"}>
+        <p>
+          <i>{isTyping ? "Typing" : ""}</i>
+        </p>
+      </div>
+
+      <form action="" onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type a message here and hit Enter..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
+  );
+}
+
+export default App;
